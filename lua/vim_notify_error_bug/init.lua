2c665fbda4d53eb99ef446c20e0b8c1ad1da983b
Ty Lewis <tyglewis@gmail.com>
2026-05-16 10:46:43 -0600

    Init commit

---
+++ lua/vim_notify_error_bug/init.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.exit_code = nil
+
+function M.run_job()
+	vim.fn.jobstart("exit 1", {
+		on_exit = function(_, exit_code)
+			M.exit_code = exit_code
+			vim.schedule(function()
+				vim.notify("Exit code: " .. exit_code, vim.log.levels.ERROR)
+			end)
+		end,
+	})
+end
+
+function M.wait_for_exit_code()
+	vim.fn.wait(1000, function()
+		return M.exit_code ~= nil
+	end)
+	vim.notify("Exit code: " .. M.exit_code, vim.log.levels.INFO)
+end
+
+return M
